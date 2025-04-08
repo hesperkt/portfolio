325e9595edd993ff2f695dbbc853b6b2820d14c0
hesperkt <hk3@wellesley.edu>
2025-04-08 19:41:47 -0400

    updated

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
 
     this.classList.toggle("active");
